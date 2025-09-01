1196af3f53d15ebe3db6adba28f0f09f1209cc72
leonk2025 <cgleon@razumtek.pe>
2025-09-01 09:32:47 -0500

    Update database.py

--- database.py
+++ database.py
@@ -2,13 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-# URL CORRECTA para SQLiteCloud (usando sqlitecloud:// directamente)
-SQLALCHEMY_DATABASE_URL = "sqlitecloud://csxitt7rnz.g3.sqlite.cloud:8860/proyectos.db?apikey=FmUQYOjwnVPprdgQwCPsNrwKxO3fT3N7WgQP5cJla6A"
+# URL para Turso (LibSQL)
+SQLALCHEMY_DATABASE_URL = os.getenv("TURSO_URL", "libsql://proyectos-leonk2025.aws-eu-west-1.turso.io")
 
 print(f"🔗 Conectando a: {SQLALCHEMY_DATABASE_URL}")
 
-# Crear el engine - SQLAlchemy detectará automáticamente el driver
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
