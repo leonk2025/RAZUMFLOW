c136454978bcf62b8ec40efc76423836d8154d4e
leonk2025 <cgleon@razumtek.pe>
2025-09-01 09:14:02 -0500

    Update database.py

--- database.py
+++ database.py
@@ -2,12 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-# URL de SQLiteCloud con el driver explícito
-SQLALCHEMY_DATABASE_URL = "sqlitecloud+dbapi://csxitt7rnz.g3.sqlite.cloud:8860/proyectos.db?apikey=FmUQYOjwnVPprdgQwCPsNrwKxO3fT3N7WgQP5cJla6A"
+# URL CORRECTA para SQLiteCloud (usando sqlitecloud:// directamente)
+SQLALCHEMY_DATABASE_URL = "sqlitecloud://csxitt7rnz.g3.sqlite.cloud:8860/proyectos.db?apikey=FmUQYOjwnVPprdgQwCPsNrwKxO3fT3N7WgQP5cJla6A"
 
 print(f"🔗 Conectando a: {SQLALCHEMY_DATABASE_URL}")
 
-# Crear el engine
+# Crear el engine - SQLAlchemy detectará automáticamente el driver
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
