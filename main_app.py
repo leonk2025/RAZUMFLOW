96d9db85f9abce839178f49ad68b237ca24061ce
leonk2025 <cgleon@razumtek.pe>
2025-09-07 13:27:43 -0500

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -445,7 +445,9 @@ if st.session_state.proyectos:
             if estado == Estado.OPORTUNIDAD:
                 if st.button("📊 Administrar Oportunidades", key=f"btn_{estado}", use_container_width=True):
                     st.switch_page("pages/1_Oportunidades.py")
-            else:
+            else if st.button("📊 Administrar Preventa", key=f"btn_{estado}", use_container_width=True):
+                    st.switch_page("pages/2_Preventa.py"):
+            else
                 st.button("⏳ Próximamente", key=f"btn_{estado}", disabled=True, use_container_width=True)
 
 else:
