04b1bf2dfa53f530a1c194c01757f7e92bfef07c
leonk2025 <cgleon@razumtek.pe>
2025-09-14 21:27:27 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///proyectos-actualizado4.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///proyectos.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, 
